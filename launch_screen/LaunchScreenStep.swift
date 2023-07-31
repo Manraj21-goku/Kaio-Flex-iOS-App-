c62a5bcf427173de1b6c464088bdc4e85c7a756f
Manraj Singh <130908667+Manraj21-goku@users.noreply.github.com>
2023-08-01 00:52:56 +0530

    Add files via upload

---
+++ launch_screen/LaunchScreenStep.swift
@@ -0,0 +1,14 @@
+//
+//  LaunchScreenStep.swift
+//  Kaio
+//
+//  Created by Manraj Singh on 19/07/23.
+//
+
+import Foundation
+
+enum LaunchScreenStep{
+    case firstStep
+    case secondStep
+    case finished
+}
